b8a0774847c773b98a34347c768df1377fdaba9a
Alex Coupe <alexander.coupe@dunnhumby.com>
2022-12-29 11:01:54 +0000

    guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+    
+        println!("Enter your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You guessed the number!");
+                break;
+            }
+        };
+}
+}
